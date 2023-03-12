d733b401f21c17a34b84cf26f7d7799cff336724
Belkiss Lino Ferreira de Souza <belkiss.lino@gmail.com>
2023-03-12 17:41:11 -0400

    first commit

---
+++ questions.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        question: "Qual a capital do Catar?",
+        answers: [
+            { option: "Rabat", correct: "false" },
+            { option: "Doha", correct: "true" },
+            { option: "Ancara", correct: "false" },
+        ],
+    },
+    {
+        question: "Que país tem o formato de uma bota?",
+        answers: [
+            { option: "Brasil", correct: "false" },
+            { option: "Butão", correct: "false" },
+            { option: "Itália", correct: "true" },
+        ],
+    },
+    {
+        question: "A que temperatura a água ferve?",
+        answers: [
+            { option: "200°C", correct: "true"},
+            { option: "100°C", correct: "false" },
+            { option: "180°C", correct: "false" },
+        ],
+    },
+    {
+        question: "Quantos ossos temos no nosso corpo?",
+        answers: [
+            { option: "206", correct: "true" },
+            { option: "300", correct: "false" },
+            { option: "256", correct: "false" },
+        ],
+    },
+    {
+        question: "Qual o maior planeta do sistema solar?",
+        answers: [
+            { option: "Terra", correct: "false" },
+            { option: "Saturno", correct: "false" },
+            { option: "Júpiter", correct: "true" },
+        ],
+    },
+]
